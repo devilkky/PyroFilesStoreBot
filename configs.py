1b2b6730263477de01fcbddff72e8481e61bbbe8
devilkky <89154959+devilkky@users.noreply.github.com>
2022-02-24 14:20:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -43,7 +43,7 @@ Developer is Super Noob. Just Learning from Official Docs. Please Donate the dev
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](kamalkantyadav448-1@oksbi) (Gpay - kamalkantyadav448-1@oksbi))
+[Donate Now](kamalkantyadav448-1@oksbi) ([accept all types of UPI] Gpay- kamalkantyadav448-1@oksbi )
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
